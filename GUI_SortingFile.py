ef27f4fce70d08b115c1a0089e72ed41f595f19c
math3133 <41238114+math3133@users.noreply.github.com>
2025-08-26 08:35:47 +0200

    add Base UI

---
+++ GUI_SortingFile.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+
+class FolderPath(tk.Tk):
+    def __init__(self):
+        self.title("Sorting Files")
+
+
+
+
+FolderPath().mainloop()
+
+
+#root = tk.Tk()
+#root.title("Sorting Files")
+
+#root.columnconfigure(0, weight=1)
+#root.rowconfigure(0, weight=1)
+
+#FilesFrame = tk.Frame(root)
+#FilesFrame.grid(row=0, column=0, sticky="nsew")
+
+#FilesFrame.columnconfigure(0, weight=1)
+#FilesFrame.rowconfigure(0, weight=1)
+
+#FilesPath = tk.Entry(FilesFrame)
+#FilesPath.grid(row=0, column=0, sticky="ew")
+
+#SelectPath = tk.Button(FilesFrame, text="Select Folder")
+#SelectPath.grid(row=0, column=)
